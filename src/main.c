63623f1364b06d41060ee567656e917a86590445
Bananacocodrilo <you@example.com>
2019-09-06 07:21:30 +0200

    :sprkles: initialize nvs

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
 
 #include "matrix.h"
 
+#include <nvs_flash.h>
+
 void app_main() {
-	// Reset the RTC pins
+	esp_err_t ret;
+
+	// Reset the RTC pins and prepare the matrix
 	rtc_matrix_deinit();
+	matrix_setup();
+
+	// Initialize NVS.
+	ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+		ESP_ERROR_CHECK (nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
 
 }
