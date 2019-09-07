9e5ab402b2e0740d8bb8cae1ba2a3d16d564547f
bananacocodrilo <antonio.mkp@gmail.com>
2019-09-07 23:34:49 +0200

    :alembic: Testing matrix pin config

--- src/keyboard_config.c
+++ src/keyboard_config.c
@@ -2,10 +2,10 @@
 
 const gpio_num_t MATRIX_COLS_PINS[] = { 
   GPIO_NUM_4, GPIO_NUM_27, GPIO_NUM_26, GPIO_NUM_39, GPIO_NUM_25, GPIO_NUM_36 
-  };
+};
 const gpio_num_t MATRIX_ROWS_PINS[] = { 
   GPIO_NUM_32, 
   GPIO_NUM_33, 
   GPIO_NUM_34, 
   GPIO_NUM_35 
-  };
+};
--- src/matrix.c
+++ src/matrix.c
@@ -120,7 +120,7 @@ void matrix_setup(void) {
 	for(uint8_t col=0; col < MATRIX_ROWS; col++) {
 
 		gpio_pad_select_gpio(MATRIX_COLS_PINS[col]);
-		gpio_set_direction(MATRIX_COLS_PINS[col], GPIO_MODE_INPUT_OUTPUT);
+		gpio_set_direction(MATRIX_COLS_PINS[col], GPIO_MODE_INPUT);
 		gpio_set_drive_capability(MATRIX_COLS_PINS[col],GPIO_DRIVE_CAP_0);
 		gpio_set_level(MATRIX_COLS_PINS[col], 0);
 
