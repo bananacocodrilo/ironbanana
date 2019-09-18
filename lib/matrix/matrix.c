ee16c1c3709d3d6281681117ab06ff07045c0912
Bananacocodrilo <antonio.mkp@gmail.com>
2019-09-18 17:24:21 +0200

    :bug: Scan matrix working with real kb

--- lib/matrix/matrix.c
+++ lib/matrix/matrix.c
@@ -6,44 +6,65 @@ uint8_t matrix_state[MATRIX_ROWS][MATRIX_COLS] = { 0 };
 uint8_t prev_matrix_state[MATRIX_ROWS][MATRIX_COLS] = { 0 };
 uint32_t debounce_matrix[MATRIX_ROWS][MATRIX_COLS] = { 0 };
 
+uint8_t keyboard_matrix[MATRIX_ROWS*NUM_KEYPADS][MATRIX_COLS] = { 0 }; 
 
+
+#ifdef COL2ROW
 void initialize_matrix_pins(void){
-  
-  for(uint8_t row = 0; row < MATRIX_ROWS; row++) {
-	  gpio_pad_select_gpio(MATRIX_ROWS_PINS[row]);
-	
-	#ifdef COL2ROW
-		gpio_set_direction(MATRIX_ROWS_PINS[row], GPIO_MODE_INPUT_OUTPUT);
-	#else
-		gpio_set_drive_capability(MATRIX_ROWS_PINS[row],GPIO_DRIVE_CAP_0);
-	#endif
+	// Initializing columns
+	for (uint8_t col = 0; col < MATRIX_COLS; col++) {
 
-    gpio_set_level(MATRIX_ROWS_PINS[row], 0);
-    ESP_LOGI(MATRIX_TAG,"Row %d on GPIO %d is level %d", row, MATRIX_ROWS_PINS[row],gpio_get_level(MATRIX_ROWS_PINS[row]));
-  }
-
-  for(uint8_t col = 0; col < MATRIX_COLS; col++) {
-	  gpio_pad_select_gpio(MATRIX_COLS_PINS[col]);
-	
-	#ifdef COL2ROW
+		gpio_pad_select_gpio(MATRIX_COLS_PINS[col]);
 		gpio_set_direction(MATRIX_COLS_PINS[col], GPIO_MODE_INPUT_OUTPUT);
-	#else
-		gpio_set_drive_capability(MATRIX_COLS_PINS[col],GPIO_DRIVE_CAP_0);
-	#endif
+		gpio_set_level(MATRIX_COLS_PINS[col], 0);
 
-    gpio_set_level(MATRIX_COLS_PINS[col], 0);
-    ESP_LOGI(MATRIX_TAG,"Col %d on GPIO %d is level %d", col, MATRIX_COLS_PINS[col],gpio_get_level(MATRIX_COLS_PINS[col]));
-  }
+		ESP_LOGI(GPIO_TAG,"%d is level %d", MATRIX_COLS_PINS[col],
+				gpio_get_level(MATRIX_COLS_PINS[col]));
+	}
 
+	// Initializing rows
+	for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+
+		gpio_pad_select_gpio(MATRIX_ROWS_PINS[row]);
+		gpio_set_direction(MATRIX_ROWS_PINS[row], GPIO_MODE_INPUT_OUTPUT);
+		gpio_set_drive_capability(MATRIX_ROWS_PINS[row], GPIO_DRIVE_CAP_0);
+		gpio_set_level(MATRIX_ROWS_PINS[row], 0);
+
+		ESP_LOGI(GPIO_TAG,"%d is level %d", MATRIX_ROWS_PINS[row],
+				gpio_get_level(MATRIX_ROWS_PINS[row]));
+	}
 
 }
 
+#else
+
+void initialize_matrix_pins(void){
+	for(uint8_t row=0; row < MATRIX_ROWS; row++) {
+
+		gpio_pad_select_gpio(MATRIX_ROWS_PINS[row]);
+		gpio_set_direction(MATRIX_ROWS_PINS[row], GPIO_MODE_INPUT_OUTPUT);
+		gpio_set_level(MATRIX_ROWS_PINS[row], 0);
+		ESP_LOGI(MATRIX_TAG,"%d is level %d",MATRIX_ROWS_PINS[row],gpio_get_level(MATRIX_ROWS_PINS[row]));
+	}
+
+	// Initializing columns
+	for(uint8_t col=0; col < MATRIX_ROWS; col++) {
+
+		gpio_pad_select_gpio(MATRIX_COLS_PINS[col]);
+		gpio_set_direction(MATRIX_COLS_PINS[col], GPIO_MODE_INPUT_OUTPUT);
+		gpio_set_drive_capability(MATRIX_COLS_PINS[col],GPIO_DRIVE_CAP_0);
+		gpio_set_level(MATRIX_COLS_PINS[col], 0);
+
+		ESP_LOGI(MATRIX_TAG,"%d is level %d",MATRIX_COLS_PINS[col],gpio_get_level(MATRIX_COLS_PINS[col]));
+	}
+}
+#endif
 
 
 
 void scan_matrix_pins(){
-uint8_t curState = 0;
-uint32_t execMillis = esp_timer_get_time() / 1000;
+  uint8_t curState = 0;
+  uint32_t execMillis = esp_timer_get_time() / 1000;
 
 #ifdef COL2ROW
 	// Set a column as high.
@@ -75,10 +96,13 @@ uint32_t execMillis = esp_timer_get_time() / 1000;
 	// Set a row as high.
 	for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
 		gpio_set_level(MATRIX_ROWS_PINS[row], 1);
-	
+
 		// Check each row
 		for (uint8_t col = 0; col < MATRIX_COLS; col++) {
 			curState = gpio_get_level(MATRIX_COLS_PINS[col]);
+      
+    // ESP_LOGI(MATRIX_TAG,"Row %d on GPIO %d is level %d", row, MATRIX_ROWS_PINS[row],gpio_get_level(MATRIX_ROWS_PINS[row]));
+    //   ESP_LOGI(MATRIX_TAG,"Col %d Row %d is level %d", col, row, curState);
 
 			// If the previous state was different restart the debounce
 			if (prev_matrix_state[row][col] != curState) {
@@ -96,6 +120,7 @@ uint32_t execMillis = esp_timer_get_time() / 1000;
 		gpio_set_level(MATRIX_ROWS_PINS[row], 0);
 	}
 #endif
+}
 
 
 void compose_keyboard_matrix(void){
@@ -122,6 +147,6 @@ void log_matrix_state(){
       printf("| %s ", ((keyboard_matrix[row][col] == 1) ? "*": "\u2022"));
     }
     printf("|\n|---|---|---|---|---|---|\n");
-}
+  }
 
 }
