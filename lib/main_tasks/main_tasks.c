8464928d1fa1f7e3995fbc850c18d7abdb5eb100
Bananacocodrilo <antonio.mkp@gmail.com>
2019-09-18 12:38:34 +0200

    :pencil: Add description for keypress task

--- lib/main_tasks/main_tasks.c
+++ lib/main_tasks/main_tasks.c
@@ -1,6 +1,25 @@
 #include "main_tasks.h"
 
-// checking the state of each key in the matrix
+/**
+ * Adds the keypresses to the reports queues (master pad task)
+ * Function flow:
+ *  - Get complete matrix
+ *    - Scan matrix
+ *    - TODO: Mix with last slave report 
+ * 
+ *  - For each key: Compare if status changed 
+ *    - Didnt change: Skip
+ *    - 0 to 1: 
+ *      - Convert to keycode taking layer and transparencies 
+ *        into account. 
+ *      - Check for layer modifications (? break?)
+ *      - Add keycode to held keys on position (was something there? release and print error)
+ *      - Add keycode to next report
+ *    - 1 to 0:
+ *      - Retrieve keycode from held
+ *      - Add keycode(release) to next report
+ *
+ */
 void keypress_reports(void *pvParameters) {
 
 }
