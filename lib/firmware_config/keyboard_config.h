e95dea90f0c7cb8cac02e99cd5a15c866fa2833f
Bananacocodrilo <antonio.mkp@gmail.com>
2019-09-18 17:21:48 +0200

    :truck: move keyboard_config to different forlder

--- lib/firmware_config/keyboard_config.h
+++ lib/firmware_config/keyboard_config.h
@@ -3,10 +3,21 @@
 
 #include <driver/gpio.h>
 
+#define ROW2COL // or COL2ROW
+
+
+// Keypad configuration
+#define KEYPAD_POSITION 1
+
 #define MATRIX_COLS 2
 #define MATRIX_ROWS 2
 
 extern const gpio_num_t MATRIX_COLS_PINS[];
 extern const gpio_num_t MATRIX_ROWS_PINS[];
+
+
+// Complete Keyboard configuration
+#define NUM_KEYPADS 1
+
 
 #endif
